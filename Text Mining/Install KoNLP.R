870c300a177b97efff6580eec4e9fc746402c87d
SeungGu Kang <63611804+kang9366@users.noreply.github.com>
2020-10-28 15:15:53 +0900

    add 'if'

--- Text Mining/Install KoNLP.R
+++ Text Mining/Install KoNLP.R
@@ -1,13 +1,13 @@
-install.packages("multilinguer")
-install.packages("remotes")
+if(!require(multilinguer)){install.packages("multilinguer")}
+if(!require(remotes)){install.packages("remotes")}
+if(!require(devtools)){install.packages("devtools")}
 install.packages(c('stringr','hash','tau','Sejong','RSQLite','devtools'),type="binary")
 install.packages(c("remotes","ggplot2", "data.table", "rlang"))
+install_jdk()
 
 library(multilinguer)
 library(rJava)
 library(KoNLP)
-
-install_jdk()
 
 devtools::install_github('haven-jeon/NIADic/NIADic', build_vignettes=TRUE)
 buildDictionary(ext_dic = "woorimalsam") # "woorimalsam" dic을 불러옵니다
